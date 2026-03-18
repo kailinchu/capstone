06269f59a6fea1f636d6450135f0d0f31bfe1232
DorianKnight <34255884+DorianKnight@users.noreply.github.com>
2026-03-17 21:23:00 -0400

    Update autoquet_grapher.py

--- pressure_plotting/autoquet_grapher.py
+++ pressure_plotting/autoquet_grapher.py
@@ -9,6 +9,7 @@ import matplotlib.animation as animation
 import time
 import serial
 import struct
+import keyboard
 
 time_series = []
 max_columns = 2 # Determines how many rows we will have
@@ -21,8 +22,8 @@ quantities_to_graph = [
     # Plot 1: Cuff pressure with set point pressure
     [["Cuff pressure", "black"], ["Set point pressure", "blue"]],
 
-    # Plot 2: BNO accelerometer readings
-    [["BNO_X", "blue"], ["BNO_Y", "red"], ["BNO_Z", "green"]],
+    # # Plot 2: BNO accelerometer readings
+    # [["BNO_X", "blue"], ["BNO_Y", "red"], ["BNO_Z", "green"]],
 
     # Plot 3: Cuff pressure after high pass filter
     [["HPF cuff pressure", "black"]],
@@ -39,23 +40,30 @@ quantities_to_graph = [
     # Plot 7: Motor PWM and Solenoid state
     [["Motor PWM", "red"], ["Solenoid state", "blue"]],
 
+    # Plot 8: Initial search state
+    [["Initial search state", "red"]],
 
-    # [["Test measurement1", "green"]],
-    # [["Test measurement2", "red"]],
-    # [["Test measurement3", "orange"]],
-    # [["Test measurement4", "purple"]],
-    # [["Test measurement5", "green"], ["Completely fake measurement", "pink"]],
-    # [["Test measurement5", "green"], ["Completely fake measurement", "pink"]],
+    # Plot 9: Ideal pressure found
+    [["Ideal pressure found", "blue"]],
+    
+    # Plot 10: Button status
+    [["Increase signal", "green"], ["Decrease signal", "red"], ["Automode signal", "black"]],
+
+    # Plot 11: Operating status
+    [["Operation mode", "black"]],
+
+    # Plot 12: Time
+    # [["Time", "red"]],
 ]
 
 graph_limits = [
     # Format: window, y_lower, y_upper, title
 
     # Plot 1: Cuff pressure with set point pressure
-    {"window":100, "y_lower":-0.1, "y_upper":150, "graph title":"Cuff pressure vs time",    "graph y_label": "Cuff pressure (mmHg)", "plot start": 0, "advance by": 20},
+    {"window":100, "y_lower":-10, "y_upper":350, "graph title":"Cuff pressure vs time",    "graph y_label": "Cuff pressure (mmHg)", "plot start": 0, "advance by": 20},
 
-    # Plot 2: BNO accelerometer readings
-    {"window": 60, "y_lower": -10, "y_upper": 10, "graph title": "HPF Accelerometry vs time", "graph y_label": "Acceleration (m/s^2)", "plot start": 0, "advance by": 20},
+    # # # Plot 2: BNO accelerometer readings
+    # # {"window": 60, "y_lower": -10, "y_upper": 10, "graph title": "HPF Accelerometry vs time", "graph y_label": "Acceleration (m/s^2)", "plot start": 0, "advance by": 20},
 
     # Plot 3: Cuff pressure after high pass filter
     {"window": 60, "y_lower": -0.1, "y_upper": 5, "graph title": "HPF cuff pressure vs time", "graph y_label": "Filtered cuff pressure (mmHg)", "plot start": 0, "advance by": 20},
@@ -64,13 +72,29 @@ graph_limits = [
     {"window": 60, "y_lower": -0.1, "y_upper": 2, "graph title": "Disruption boolean signals vs time", "graph y_label": "Signal level (unitless)", "plot start": 0, "advance by": 20},
 
     # Plot 5: Quantized blood flow strength
-    {"window": 60, "y_lower": 0, "y_upper": 5000, "graph title": "Measured blood flow summation", "graph y_label": "Qauntized pulse strength (unitless)", "plot start": 0, "advance by": 20},
+    {"window": 60, "y_lower": -10, "y_upper": 2000, "graph title": "Measured blood flow summation", "graph y_label": "Quantized pulse strength (unitless)", "plot start": 0, "advance by": 20},
+    # {"window": 60, "y_lower": -10, "y_upper": 70000, "graph title": "Measured blood flow summation", "graph y_label": "Quantized pulse strength (unitless)", "plot start": 0, "advance by": 20},
 
     # Plot 6: Subject's bleeding status
     {"window": 60, "y_lower": -0.1, "y_upper": 1.1, "graph title": "Subect bleeding status", "graph y_label": "Bloodflow boolean (unitless)", "plot start": 0, "advance by": 20},
 
     # Plot 7: Motor and solenoid drive sitnals
     {"window": 60, "y_lower": -3, "y_upper": 2, "graph title": "Motor and Solenoid state", "graph y_label": "Signal level (unitless)", "plot start": 0, "advance by": 20},
+
+    # Plot 8: Initial search state
+    {"window": 60, "y_lower": -1, "y_upper": 4, "graph title": "Initial search state", "graph y_label": "State variable (unitless)", "plot start": 0, "advance by": 20},
+
+    # Plot 9: Ideal pressure found
+    {"window": 60, "y_lower": -1, "y_upper": 2, "graph title": "Ideal pressure found boolean", "graph y_label": "Indicator (unitless)", "plot start": 0, "advance by": 20},
+
+    # Plot 10: Button status
+    {"window": 60, "y_lower": -3, "y_upper": 4, "graph title": "Manual mode buttons", "graph y_label": "State (unitless)", "plot start": 0, "advance by": 20},
+
+    # Plot 11: Operating mode
+    {"window": 60, "y_lower": -1, "y_upper": 2, "graph title": "Operating status", "graph y_label": "State (unitless)", "plot start": 0, "advance by": 20},
+
+    # Plot 12: Time
+    # {"window": 100, "y_lower": 0, "y_upper": 100000, "graph title": "Time", "graph y_label": "State (unitless)", "plot start": 0, "advance by": 10},
 ]
 
 # Dictionary storing all values read in from Serial buffer
@@ -88,11 +112,21 @@ data_list_complete = {"Cuff pressure": 0,
                       "Bloodflow threshold": 0,
                       "Motor PWM": 0,
                       "Solenoid state": 0,
+                      "Initial search state": 0,
+                      "Ideal pressure found": 0,
+                      "Increase signal": 0,
+                      "Decrease signal": 0,
+                      "Automode signal": 0,
+                      "Operation mode": 0,
+                      "Milliseconds": 0,
+                      "Seconds": 0,
+                      "Minutes": 0,
+                      "Hours":0,
                       }
 
 num_of_subplots = len(quantities_to_graph)
 incident_time = time.perf_counter()
-ser = serial.Serial('COM8', 9600, timeout=1)
+ser = serial.Serial('COM10', 115200, timeout=1)
 
 def grid_formater(num_of_subplots):
     # Uses global "quantities_to_graph" variable and comes up with the number of rows and columns needed to represent all data
@@ -115,7 +149,6 @@ class grapher():
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
-
         self.fig, self.ax = plt.subplots(rows,columns,sharex=False)
         self.remove_unused_plots()
         self.fig.set_size_inches(15, 9)
@@ -176,7 +209,10 @@ class grapher():
 
     # Read in data from serial
     def read_data(self, elapsed_time):
+        ser.write(bytes("R", "utf-8")) # Request data
+        # print("Requesting data")
         if (ser.in_waiting > 0):
+            # print("Data received")
             time_series.append(elapsed_time)
             cuff_pressure_raw = ser.read(4) # Read in 4 bytes to reconstruct float
             pressure_set_point_raw = ser.read(4)
@@ -192,6 +228,17 @@ class grapher():
             bloodflow_threshold_raw = ser.read(2)
             motor_pwm_raw = ser.read(4)
             solenoid_state_raw = ser.read(1)
+            initial_search_state_raw = ser.read(1)
+            ideal_pressure_found_raw = ser.read(1)
+            increase_signal_raw = ser.read(1)
+            decrease_signal_raw = ser.read(1)
+            automode_signal_raw = ser.read(1)
+            operation_mode_raw = ser.read(1)
+            milliseconds_raw = ser.read(1)
+            seconds_raw = ser.read(1)
+            minutes_raw = ser.read(1)
+            hours_raw = ser.read(1)
+            
 
             # Unpack serial reads only if data was received
             if (cuff_pressure_raw != ''):
@@ -204,11 +251,27 @@ class grapher():
                 data_list_complete["Motor active"] = struct.unpack('<?', motor_active_raw)[0]
                 data_list_complete["Movement detected"] = struct.unpack('<?', accelerometer_active_raw)[0]
                 data_list_complete["Summation paused"] = struct.unpack('<?', pause_summation_raw)[0]
-                data_list_complete["Bloodflow summation"] = struct.unpack('<h', bloodflow_summation_raw)[0]
+                data_list_complete["Bloodflow summation"] = struct.unpack('<H', bloodflow_summation_raw)[0]
                 data_list_complete["Bloodflow boolean"] = struct.unpack('<?', bloodflow_boolean_raw)[0]
-                data_list_complete["Bloodflow threshold"] = struct.unpack('<h', bloodflow_threshold_raw)[0]
+                data_list_complete["Bloodflow threshold"] = struct.unpack('<H', bloodflow_threshold_raw)[0]
                 data_list_complete["Motor PWM"] = struct.unpack('<f', motor_pwm_raw)[0]/255
                 data_list_complete["Solenoid state"] = struct.unpack('<?', solenoid_state_raw)[0] - 2
+                data_list_complete["Initial search state"] = struct.unpack('<B', initial_search_state_raw)[0]
+                data_list_complete["Ideal pressure found"] = struct.unpack('<?', ideal_pressure_found_raw)[0]
+                data_list_complete["Increase signal"] = struct.unpack('<?', increase_signal_raw)[0]
+                data_list_complete["Decrease signal"] = struct.unpack('<?', decrease_signal_raw)[0] - 2
+                data_list_complete["Automode signal"] = struct.unpack('<?', automode_signal_raw)[0] + 2
+                data_list_complete["Operation mode"] = struct.unpack('<B', operation_mode_raw)[0]
+                data_list_complete["Milliseconds"] = struct.unpack('<B', milliseconds_raw)[0]
+                data_list_complete["Seconds"] = struct.unpack('<B', seconds_raw)[0]
+                data_list_complete["Minutes"] = struct.unpack('<B', minutes_raw)[0]
+                data_list_complete["Hours"] = struct.unpack('<B', hours_raw)[0]
+
+
+                # print(data_list_complete["Cuff pressure"])
+                # print(data_list_complete["Set point pressure"])
+                # print(data_list_complete["Bloodflow summation"])
+                print(data_list_complete["Milliseconds"], data_list_complete["Seconds"], data_list_complete["Minutes"], data_list_complete["Hours"])
 
                 # Only append the data requested to be graphed
                 for i in range(len(self.presented_data)):
@@ -253,5 +316,12 @@ graphObj = grapher(rows, columns)
 graphObj.graph_init()
 plot_animation = animation.FuncAnimation(graphObj.fig, animate, blit=True, interval=0.1)
 plt.show()
+print("Just before closing")
+
+ser.close()
+print("Serial port closed")
+
+
+
 
 
