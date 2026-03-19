af1e5bf48f315a4c899277510f9031abd9f174ac
Jocelyn Xu <xu90@mcmaster.ca>
2026-03-19 10:57:49 -0400

    Update pulsatile pump with bpm buttons

--- pulsatile_pump/pulsatile_pump.ino
+++ pulsatile_pump/pulsatile_pump.ino
@@ -8,56 +8,109 @@
 
 #include <Wire.h>
 
+
 #define MOTOR_PWM 14
 #define MOTOR_EN 27
-#define A1 2
-#define A0 4
-#define PRESSURE_TRANS 32
+
+#define BUTTON_LOW 13
+#define LED_LOW 25
+#define BUTTON_MID 32
+#define LED_MID 26
+#define BUTTON_HIGH 21
+#define LED_HIGH 23
 
 // declare pulse wave flow vector
 // data from http://haemod.uk/aortic
 int flowVector[100] = {5,54,107,158,201,233,251,255,253,243,230,213,196,178,162,146,132,119,107,95,82,67,47,22,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
-int pulseRate = 60;
+//int pulseRate = 60;
+int pulseRates[8] = {60,100,140};
 int pulseFraction = 25;
 int baseline = 5;
 
 void setup() {
-  Serial.begin(115200);         
+  Serial.begin(115200);     
 
-  // INITIALIZE ANALOG OUT OF PULSE (FOR COMPARING PULSE WAVEFORM TO PRESSURE WAVEFORM)
-  pinMode(A1, OUTPUT);
-  // INITIALIZE DIGITAL OUT TO SYNC PULSE CYCLE TO EXTERNAL EQUIPMENT
-  pinMode(13, OUTPUT);
+  // INITIALIZE PERIPHERALS
+  pinMode(BUTTON_LOW, INPUT_PULLUP);
+  pinMode(BUTTON_MID, INPUT_PULLUP);
+  pinMode(BUTTON_HIGH, INPUT_PULLUP);
   // initialize digital pin for relay of solenoid valve as an output.
-  pinMode(A0, OUTPUT);
-
-  
-
+  pinMode(LED_LOW, OUTPUT);
+  pinMode(LED_MID, OUTPUT);
+  pinMode(LED_HIGH, OUTPUT);
 }
-int adcValue;
+uint8_t i;
+bool low_reading = 0;
+bool mid_reading = 0;
+bool high_reading=0;
+int pulseRate = 0;
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(MOTOR_EN, HIGH); // enable forward moving of motor
-  uint8_t i;
-
-  //myMotor->run(FORWARD);
   int bufferLength = 125-pulseFraction;
   digitalWrite(13, HIGH);
-  for (i=0; i<bufferLength; i++) {
-    int currentFlow = flowVector[i];
-    if (currentFlow < baseline) currentFlow = baseline;
-
-    analogWrite(A1, currentFlow);
-    if (i>0) digitalWrite(13, LOW); //
+  if (pulseRate!=0){
+    digitalWrite(MOTOR_EN, HIGH); // enable forward moving of motor
+    for (i=0; i<bufferLength; i++) {
+      int currentFlow = flowVector[i];
+      if (currentFlow < baseline) currentFlow = baseline;
+      // analogWrite(A1, currentFlow);
+      // if (i>0) digitalWrite(13, LOW); //
+      analogWrite(MOTOR_PWM,currentFlow);
     
-    analogWrite(MOTOR_PWM,currentFlow);
-    //Serial.println(currentFlow);
-    // pressure gauge measurement
-    adcValue = analogRead(PRESSURE_TRANS);
-    Serial.println(adcValue);
-    // currently delays between readings, but we can smoothen this out w non time blocking delays
-   
-    delay(60*1000/(pulseRate*bufferLength));
+      delay(60*1000/(pulseRate*bufferLength));
+    }
   }
-  
+  else{
+    digitalWrite(MOTOR_EN, LOW); // stop motor
+  }
+
+  if (digitalRead(BUTTON_LOW) == LOW && !low_reading){
+    low_reading = 1;
+    delay(20);
+    
+    pulseRate = 60;
+    digitalWrite(LED_LOW, HIGH);
+    digitalWrite(LED_MID, LOW);
+    digitalWrite(LED_HIGH,LOW);
+    // pulseRate = pulseRates[pulseRateIndex];
+    Serial.print("60 BPM");
+  }
+  else{
+    if (digitalRead(BUTTON_LOW) == HIGH){
+    low_reading = 0;
+    }
+  }
+
+  if (digitalRead(BUTTON_MID) == LOW && !mid_reading){
+    mid_reading = 1;
+    delay(20);
+    
+    pulseRate = 100;
+    digitalWrite(LED_LOW, LOW);
+    digitalWrite(LED_MID, HIGH);
+    digitalWrite(LED_HIGH,LOW);
+    // pulseRate = pulseRates[pulseRateIndex];
+    Serial.print("100 BPM");
+  }
+  else{
+    if (digitalRead(BUTTON_MID) == HIGH){
+    mid_reading = 0;
+    }
+  }
+
+  if (digitalRead(BUTTON_HIGH) == LOW && !high_reading){
+    high_reading = 1;
+    delay(20);
+    
+    pulseRate = 140;
+    digitalWrite(LED_LOW, LOW);
+    digitalWrite(LED_MID, LOW);
+    digitalWrite(LED_HIGH,HIGH);
+    Serial.print("120 BPM");
+  }
+  else{
+    if (digitalRead(BUTTON_HIGH) == HIGH){
+    high_reading = 0;
+    }
+  }
 }
